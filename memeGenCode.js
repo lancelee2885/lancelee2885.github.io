60cf5409f9f93eb924a71b71144c4da8a315131b
lancelee2885 <lancelee2885@gmail.com>
2021-06-09 23:38:41 -0700

    meme

---
+++ memeGenCode.js
@@ -0,0 +1,68 @@
+window.onload = function(){
+    let form = document.getElementById("form")
+    let imageCanvas = document.getElementById("memeCanvas");
+    let allMeme = document.querySelectorAll("meme") ;
+    let removeAll = document.getElementById("removeAll");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+    
+        let newMeme = document.createElement("p");
+        newMeme.setAttribute("id", "meme");
+        
+        let imgSource = document.getElementById("imageSource").value
+        
+        newMeme.className = "container";
+        newMeme.style.backgroundImage = "url('" + imgSource + "')";
+        newMeme.style.backgroundSize = "cover";
+        newMeme.style.backgroundRepeat = "no-repeat";
+        newMeme.style.backgroundPosition = "center";
+        newMeme.style.display = "flex";
+        newMeme.style.flexDirection = "column";
+        newMeme.style.height = '300px';
+        newMeme.style.width = '300px';
+        newMeme.style.justifyContent = "space-between";
+        newMeme.style.alignItems = "center";
+        
+
+        
+        let topText = document.createElement("div");
+        topText.innerText = document.getElementById("topText").value;
+        topText.setAttribute("id", "memeText");
+        
+        topText.style.color = "white";
+        topText.style.fontWeight = "bold";
+        topText.style.textShadow = '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000';
+
+        
+    
+        let bottomText = document.createElement("div");
+        bottomText.innerText = document.getElementById("bottomText").value;
+        bottomText.setAttribute("id", "memeText");
+
+        bottomText.style.color = "white";
+        bottomText.style.fontWeight = "bold";
+        bottomText.style.textShadow = '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000';
+        
+
+    
+            
+        imageCanvas.appendChild(newMeme);
+        newMeme.appendChild(topText);
+        newMeme.appendChild(bottomText);
+
+        form.reset();
+
+
+        
+    })
+
+    imageCanvas.addEventListener('click', function(event){
+        if (event.target.tagName.toLowerCase() === 'p')  {
+            event.target.remove();
+        }
+
+    })
+    
+    
+}
